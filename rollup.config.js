15b653d6325923e13d2ba653a7f994ed4657dc98
Daniel Mulholland <dan.mulholland@gmail.com>
2026-02-28 21:10:04 +1000

    chore: remove source files and use only npm dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,90 +1,95 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import copy from 'rollup-plugin-copy';
-import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
-
-import { readdirSync } from 'fs';
 
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
-
+import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
+import copy from 'rollup-plugin-copy';
+// import { generateSW } from 'rollup-plugin-workbox';
+// import path from 'path';
 const outputDir = 'bundle';
 
-const locales = readdirSync('src/locales').map(locale => ({
-  input: `src/locales/${locale}`,
-  output: {
-    sourcemap: true, // Add source map to build output
-    format: 'es', // ES module type export
-    file: `${`dist/locales/${locale}`.slice(0, -3)}.js`, // Keep filename
-  },
-  preserveEntrySignatures: 'strict', // leaves export of the plugin entry point
-
-  plugins: [nodeResolve(), typescript({ declaration: false })],
-}));
-
 export default [
-  ...locales,
-  {
-    input: 'src/oscd-shell.ts',
-    output: {
-      sourcemap: true, // Add source map to build output
-      format: 'es', // ES module type export
-      dir: 'dist', // The build output folder
-      // preserveModules: true,  // Keep directory structure and files
-    },
-    preserveEntrySignatures: 'strict', // leaves export of the plugin entry point
-    plugins: [
-      /** Resolve bare module imports */
-      nodeResolve(),
-      typescript(),
-      importMetaAssets({ warnOnError: true }),
-    ],
-  },
-  {
-    input: 'src/foundation.ts',
-    output: {
-      sourcemap: true, // Add source map to build output
-      format: 'es', // ES module type export
-      dir: 'dist', // The build output folder
-      preserveModules: true, // Keep directory structure and files
-    },
-    preserveEntrySignatures: 'strict', // leaves export of the plugin entry point
-    plugins: [
-      /** Resolve bare module imports */
-      nodeResolve(),
-      typescript(),
-      importMetaAssets({ warnOnError: true }),
-    ],
-  },
   {
     input: 'index.html',
+    external: [
+      './plugins/openscd.github.io/_snowpack/pkg/lit-translate.js',
+      './plugins/openscd.github.io/openscd/dist/translations/loader.js',
+    ],
     plugins: [
       html({
-        input: 'index.html',
         minify: true,
+        // injectServiceWorker: true,
+        // serviceWorkerPath: "bundle/sw.js",
+      }),
+      /** Resolve bare module imports */
+      nodeResolve(),
+
+      /** Bundle assets references via import.meta.url */
+      importMetaAssets({ warnOnError: true }),
+
+      copy({
+        targets: [
+          {
+            src: 'node_modules/ace-builds/src-noconflict/*.js',
+            dest: `${outputDir}/ace`,
+          },
+        ],
+        verbose: true,
+        flatten: true,
       }),
       copy({
         targets: [
           {
-            src: [
-              'favicon.ico',
-              'fonts',
-              'background.svg',
-              '*.css',
-              'oscd-logo.jpeg',
-            ],
+            src: ['favicon.ico', 'fonts', 'background.svg', '*.css'],
+            dest: `${outputDir}/assets`,
+          },
+          {
+            src: ['plugins'],
             dest: `${outputDir}`,
           },
+          // Add more patterns if you have more assets
         ],
         verbose: true,
         flatten: false,
       }),
-      nodeResolve(),
-      importMetaAssets({ warnOnError: true }),
+
+      /** Create and inject a service worker */
+      // generateSW({
+      //   globIgnores: ["polyfills/*.js", "nomodule-*.js"],
+      //   navigateFallback: "/index.html",
+      //   // where to output the generated sw
+      //   swDest: path.join(outputDir, "sw.js"),
+      //   // directory to match patterns against to be precached
+      //   globDirectory: path.join(outputDir),
+      //   // cache any html js and css by default
+      //   globPatterns: [
+      //     "*.{html,js,css}",
+      //     "**/*.{html,js,css,webmanifest,ttf,ico,woff,woff2,svg,json}",
+      //   ],
+      //   skipWaiting: true,
+      //   clientsClaim: true,
+      //   // Check for updates every time the page loads
+      //   cleanupOutdatedCaches: true,
+      //   runtimeCaching: [
+      //     { urlPattern: "polyfills/*.js", handler: "CacheFirst" },
+      //   ],
+
+      //   // For reasons, which remain unexplained, the workbox plugin does not
+      //   // automatically include the material symbols font, so we add it manually
+      //   // to the precache manifest.
+      //   additionalManifestEntries: [
+      //     {
+      //       url: "assets/fonts/material-symbols-outlined.woff2",
+      //       revision: null,
+      //     },
+      //   ],
+      // }),
     ],
     output: {
       dir: outputDir,
       format: 'es',
-      sourcemap: true,
+      sourcemap: false,
+      entryFileNames: '[name]-[hash].js',
+      chunkFileNames: '[name]-[hash].js',
     },
   },
 ];
